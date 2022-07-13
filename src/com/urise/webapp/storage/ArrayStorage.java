e5c1af109fd84e699d87a3618a137c359f2dbc31
IvanZhukovsky_MacBookAir <izhukowsky@gmail.com>
2022-07-13 18:32:20 +0500

    разложил по пакетам как в уроке

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,4 +1,6 @@
-import com.sun.source.util.SourcePositions;
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
 
@@ -9,12 +11,12 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     int size = 0;
 
-    void clear() {
+    public void clear() {
         Arrays.fill(storage, 0, size - 1, null);
         size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
         if (size < 10000) {
             if (checkPresent(r.uuid) == -1) {
                 storage[size] = r;
@@ -27,7 +29,7 @@ public class ArrayStorage {
         }
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         int index = checkPresentAndPrint(uuid);
         if (index != -1) {
             return storage[index];
@@ -35,7 +37,7 @@ public class ArrayStorage {
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         int index = checkPresentAndPrint(uuid);
         if (index != -1) {
             System.arraycopy(storage, index + 1, storage, index, size - index - 1);
@@ -44,7 +46,7 @@ public class ArrayStorage {
         }
     }
 
-    void update(Resume resume) {
+    public void update(Resume resume) {
         int index = checkPresentAndPrint(resume.uuid);
         if (index != -1) {
             storage[index] = resume;
@@ -54,15 +56,15 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 
-    int checkPresent(String uuid) {
+    private int checkPresent(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 return i;
@@ -70,7 +72,7 @@ public class ArrayStorage {
         }
         return -1;
     }
-    int checkPresentAndPrint(String uuid) {
+    private int checkPresentAndPrint(String uuid) {
         int result = checkPresent(uuid);
         if (result == -1) {
             System.out.printf("Резюме %s отсуствует в базе данных%n", uuid);
