e81a5f8e2194a216d31d548739ef1485ba36a190
IvanZhukovsky_MacBookAir <izhukowsky@gmail.com>
2022-11-02 22:30:46 +0500

    ДЗ 12 версия 2

--- src/com/urise/webapp/MainStream.java
+++ src/com/urise/webapp/MainStream.java
@@ -1,7 +1,9 @@
 package com.urise.webapp;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class MainStream {
@@ -24,31 +26,23 @@ public class MainStream {
     }
 
     private static int minValue(int[] values) {
-        return IntStream.of(values).distinct().sorted().reduce(0, (acc, x) -> acc * 10 + x);
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (acc, x) -> acc * 10 + x);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        return integers.stream().collect(
-                () -> {
-                    List<Integer> chet = new ArrayList<>();
-                    List<Integer> neChet = new ArrayList<>();
-                    int summa = 0;
-                    for (Integer integer : integers) {
-                        if (integer % 2 == 0) {
-                            chet.add(integer);
-                        } else neChet.add(integer);
-                        summa += integer;
-                    }
-                    if (summa % 2 == 0) {
-                        return chet;
-                    } else
-                        return neChet;
-                },
-                (list2, integer) -> {
-
-                },
-                (list1, list2) -> {
-
-                });
+        if (integers.stream()
+                .reduce(0, (a, b) -> a + b) % 2 == 0) {
+            return integers.stream()
+                    .filter(integer -> integer % 2 == 0)
+                    .collect(Collectors.toList());
+        } else {
+            return integers.stream()
+                    .filter(integer -> integer % 2 != 0)
+                    .collect(Collectors.toList());
+        }
     }
+
 }
